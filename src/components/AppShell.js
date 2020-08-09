2e6faf79e23d4f899bf8521f4d4a0a6a28c6a3f3
JSYOvO <wotjq5113@gmail.com>
2020-08-09 16:13:57 +0900

    Commit Test2

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -45,15 +45,12 @@ class AppShell extends React.Component {
                         <MenuItem onClick = {this.handleDrawerToggle}>
                             <Link component = {RouterLink} to = '/'>
                                 홈 화면
-                                <br/>
                             </Link>
                             <Link component = {RouterLink} to = '/texts'>
                                 텍스트 관리
-                                <br/>
                             </Link>
                             <Link component = {RouterLink} to = '/words'>
                                 단어 관리
-                                <br/>
                             </Link>
                         </MenuItem>
                     </Drawer>
